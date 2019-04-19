f1b566c6bafa98adcf39b1864bdc787efe2f03c1
Christian Riggenbach <criggenbach@magahugu.ch>
2019-04-19 09:02:05 +0100

    Added logrotate

--- update.sh
+++ update.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 cp ntripcaster.conf /usr/local/ntripcaster/
+cp ntripcaster.logrotate /usr/local/ntripcaster/
 
 cp *.service /etc/systemd/system
 systemctl daemon-reload
 
 systemctl try-restart ntripcaster.service
-systemctl try-restart str2str
+systemctl try-restart str2str.service
+systemctl try-restart logrotate-ntripcaster.service
